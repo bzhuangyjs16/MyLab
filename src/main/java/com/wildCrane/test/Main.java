7847c82432707238a909cdf5c8ae62b9e3724b06
zhuangbo <bzhuangyjs16@163.com>
2021-02-06 22:17:46 +0800

    linux

--- src/main/java/com/wildCrane/test/Main.java
+++ src/main/java/com/wildCrane/test/Main.java
@@ -9,10 +9,11 @@ import java.lang.reflect.Proxy;
 
 public class Main {
     public static void main(String[] args) {
-        People boss = new Boss();
+    /*    People boss = new Boss();
         InvocationHandler assistant = new Assistant(boss);
         People proxy = (People) Proxy.newProxyInstance(boss.getClass().getClassLoader(),People.class.getInterfaces(),assistant);
-        proxy.work();
+        proxy.work();*/
+        System.out.println("hello world");
 
     }
 }
